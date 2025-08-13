e6f8ee3991469c45d7fe6414875a141fe9ffa45a
Syed Zayed Ahmed <109905969+Syedzayed@users.noreply.github.com>
2025-08-13 11:50:43 +0530

    create new page with initial design

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Card, CardHeader } from "@/components/ui/card";
+
+export default function NewTrip(){
+    return(
+            <div className="max-w-lg mx-auto mt-10">
+                <Card>
+                    <CardHeader>New Trip</CardHeader>
+                </Card>
+            </div>
+    );
+}
