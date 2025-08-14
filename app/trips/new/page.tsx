e4c0abb62a47a073225a9ef08f065a35c6d6cea0
Syed Zayed Ahmed <109905969+Syedzayed@users.noreply.github.com>
2025-08-14 21:10:49 +0530

    connect New Trip form to database and add image upload

--- app/trips/new/page.tsx
+++ app/trips/new/page.tsx
@@ -2,27 +2,45 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createTrip } from "@/lib/actions/create-trip";
 import { cn } from "@/lib/utils";
+import { UploadButton } from "@/lib/upload-thing";
+import { useState, useTransition } from "react";
+import Image from "next/image";
 
 export default function NewTrip() {
+  const [isPending, startTransition] = useTransition();
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
   return (
     <div className="max-w-lg mx-auto mt-10">
       <Card>
-        <CardHeader>New Trip</CardHeader>
+        <CardHeader> New Trip</CardHeader>
         <CardContent>
-          <form className="space-y-6">
+          <form
+            className="space-y-6"
+            action={(formData: FormData) => {
+              if (imageUrl) {
+                formData.append("imageUrl", imageUrl);
+              }
+              startTransition(() => {
+                createTrip(formData);
+              });
+            }}
+          >
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
+                {" "}
                 Title
               </label>
               <input
                 type="text"
                 name="title"
-                placeholder="Japan trip...."
+                placeholder="Japan trip..."
                 className={cn(
                   "w-full border border-gray-300 px-3 py-2",
                   "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 )}
+                required
               />
             </div>
             <div>
@@ -31,7 +49,7 @@ export default function NewTrip() {
               </label>
               <textarea
                 name="description"
-                placeholder="Trip Description..."
+                placeholder="Trip description..."
                 className={cn(
                   "w-full border border-gray-300 px-3 py-2",
                   "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -47,7 +65,6 @@ export default function NewTrip() {
                 <input
                   type="date"
                   name="startDate"
-                 
                   className={cn(
                     "w-full border border-gray-300 px-3 py-2",
                     "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -56,12 +73,12 @@ export default function NewTrip() {
               </div>
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
+                  {" "}
                   End Date
                 </label>
                 <input
                   type="date"
                   name="endDate"
-                 
                   className={cn(
                     "w-full border border-gray-300 px-3 py-2",
                     "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -69,8 +86,32 @@ export default function NewTrip() {
                 />
               </div>
             </div>
-            <Button type="submit" className="w-full" >
-                Create Trip 
+            <div>
+              <label> Trip Image</label>
+
+              {imageUrl && (
+                <Image
+                  src={imageUrl}
+                  alt="Trip Preview"
+                  className="w-full mb-4 rounded-md max-h-48 object-cover"
+                  width={300}
+                  height={100}
+                />
+              )}
+              <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  if (res && res[0].ufsUrl) {
+                    setImageUrl(res[0].ufsUrl);
+                  }
+                }}
+                onUploadError={(error: Error) => {
+                  console.error("Upload error: ", error);
+                }}
+              />
+            </div>
+            <Button type="submit" disabled={isPending} className="w-full">
+              {isPending ? "Creating..." : "Create Trip"}
             </Button>
           </form>
         </CardContent>
