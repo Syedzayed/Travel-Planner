534a6195b78af9fd963bae617563ef6c2ea8d101
Syed Zayed Ahmed <109905969+Syedzayed@users.noreply.github.com>
2025-08-19 12:14:42 +0530

    add maps tab to display selected locations

--- components/trip-detail.tsx
+++ components/trip-detail.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Trip } from "@/app/generated/prisma";
+import { Location, Trip } from "@/app/generated/prisma";
 import Image from "next/image";
 import { Calendar, MapPin, Plus } from "lucide-react";
 import Link from "next/link";
@@ -9,20 +9,24 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 import { useState } from "react";
 import Map from "@/components/map";
 import SortableItinerary from "./sortable-itinerary";
+
 export type TripWithLocation = Trip & {
   locations: Location[];
 };
 
 interface TripDetailClientProps {
-  trip: Trip;
+  trip: TripWithLocation;
 }
 
 export default function TripDetailClient({ trip }: TripDetailClientProps) {
   const [activeTab, setActiveTab] = useState("overview");
+
   return (
     <div className="container mx-auto px-4 py-8 space-y-8">
+      {" "}
       {trip.imageUrl && (
         <div className="w-full h-72 md:h-96 overflow-hidden rounded-xl shadow-lg relative">
+          {" "}
           <Image
             src={trip.imageUrl}
             alt={trip.title}
@@ -32,17 +36,17 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
           />
         </div>
       )}
-
-      <div className="bg-white p-6 shadow rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center ">
+      <div className="bg-white p-6 shadow rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center">
         <div>
           <h1 className="text-4xl font-extrabold text-gray-900">
+            {" "}
             {trip.title}
           </h1>
 
           <div className="flex items-center text-gray-500 mt-2">
             <Calendar className="h-5 w-5 mr-2" />
             <span className="text-lg">
-              {trip.startDate.toLocaleDateString()} -
+              {trip.startDate.toLocaleDateString()} -{" "}
               {trip.endDate.toLocaleDateString()}
             </span>
           </div>
@@ -50,12 +54,12 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
         <div className="mt-4 md:mt-0">
           <Link href={`/trips/${trip.id}/itinerary/new`}>
             <Button>
+              {" "}
               <Plus className="mr-2 h-5 w-5" /> Add Location
             </Button>
           </Link>
         </div>
       </div>
-
       <div className="bg-white p-6 shadow rounded-lg">
         <Tabs value={activeTab} onValueChange={setActiveTab}>
           <TabsList className="mb-6">
@@ -125,15 +129,18 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
               </div>
             </div>
           </TabsContent>
+
           <TabsContent value="itinerary" className="space-y-6">
             <div className="flex justify-between items-center mb-4">
-              <h2 className="text-2xl font-semibold">Full Itinerary</h2>
+              <h2 className="text-2xl font-semibold"> Full Itinerary</h2>
             </div>
+
             {trip.locations.length === 0 ? (
               <div className="text-center p-4">
                 <p>Add locations to see them on the itinerary.</p>
                 <Link href={`/trips/${trip.id}/itinerary/new`}>
                   <Button>
+                    {" "}
                     <Plus className="mr-2 h-5 w-5" /> Add Location
                   </Button>
                 </Link>
@@ -142,7 +149,29 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
               <SortableItinerary locations={trip.locations} tripId={trip.id} />
             )}
           </TabsContent>
+
+          <TabsContent value="map" className="space-y-6">
+            <div className="h-72 rounded-lg overflow-hidden shadow">
+              <Map itineraries={trip.locations} />
+            </div>
+            {trip.locations.length === 0 && (
+              <div className="text-center p-4">
+                <p>Add locations to see them on the map.</p>
+                <Link href={`/trips/${trip.id}/itinerary/new`}>
+                  <Button>
+                    {" "}
+                    <Plus className="mr-2 h-5 w-5" /> Add Location
+                  </Button>
+                </Link>
+              </div>
+            )}
+          </TabsContent>
         </Tabs>
+      </div>
+      <div className="text-center">
+        <Link href={`/trips`}>
+          <Button> Back to Trips</Button>
+        </Link>
       </div>
     </div>
   );
